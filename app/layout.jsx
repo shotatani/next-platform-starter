345a0b731e8fad3ead5b5da7ae2ee1926c4b5517
shotatani <139521635+shotatani@users.noreply.github.com>
2024-05-13 02:07:22 +0000

    1sttest

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,13 +5,13 @@ import { Header } from '../components/header';
 export const metadata = {
     title: {
         template: '%s | Netlify',
-        default: 'Netlify Starter'
+        default: 'エコテナーα　特設サイト'
     }
 };
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="en" data-theme="lofi">
+        <html lang="ja" data-theme="lofi">
             <head>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
             </head>
